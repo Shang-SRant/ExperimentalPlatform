54d39bb637da20e404feb12a6c48dd0873546607
Shang_SRant <shangsrant@gmail.com>
2021-05-18 11:54:21 +0800

    修订BUG

--- src/main/java/com/stq/experimentalplatform/entity/User.java
+++ src/main/java/com/stq/experimentalplatform/entity/User.java
@@ -1,5 +1,6 @@
 package com.stq.experimentalplatform.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,7 +26,7 @@ public class User implements Serializable {
     /**
      * 用户ID
      */
-    @TableId
+    @TableId(type = IdType.AUTO)
     private Integer userId;
 
     /**
